cbb46b825c8dda79c32c387a5db80587128d9f0b
agaffar <abdul.gaffar@india.semanticbits.com>
2016-12-19 20:15:36 +0530

    sample valdated organizo

--- validation.js
+++ validation.js
@@ -6,20 +6,24 @@ function validation(){
     var val2 = document.myform.age;
     if(val1.value == "" )
     {
-        document.getElementById("ful").innerHTML = "<font color='red'>enter the name</font>";
+        alert("hello");
+        document.getElementById('ful').innerHTML = '<font color="red">enter the name</p>';
     }
     else
     {
-        document.getElementById("ful").innerHTML = "<font color='green'>valid</font>";
+        alert("not null");
+
+        document.getElementById('ful').innerHTML = '<font color="green">valid</p>';
 
     }
+
     if((val2.value == "") || (val2.value <= 18) )
     {
-        document.getElementById("agVal").innerHTML = "<font color='red'>your not elgible for the operation</font>";
+        document.getElementById("agVal").innerHTML = "<font color='red'>your not elgible for the operation</p>";
     }
     else
     {
-        document.getElementById("agVal").innerHTML = "<font color='green'>your elgible for the operation</font>";
+        document.getElementById("agVal").innerHTML = "<font color='green'>your elgible for the operation</p>";
 
     }
 }
@@ -27,7 +31,7 @@ function validate()
 {
     var val1 = document.myform.fname;
 
-    if(val1.value === "" )
+    if(val1.value == "" )
     {
         alert("hello");
         document.getElementById('ful').innerHTML = '<font color="red">enter the name</p>';
